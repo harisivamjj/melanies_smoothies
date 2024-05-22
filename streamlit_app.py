bed61296074ddcfcf8b406984186b42350144b91
harisivamjj <harijj1102@gmail.com>
2024-05-22 11:41:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,38 +12,39 @@ st.write(
 name_on_order = st.text_input('Name on smoothie')
 st.write('The name on the smoothie will be ', name_on_order)
  
-cnx = st.connection("snowflake")
+cnx = st.connection("snowflake") 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
- 
-pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
- 
+
+pd_df=my_dataframe.to_pandas
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_list= st.multiselect('Choose upto 5 ingredients: ',my_dataframe,max_selections=5)
  
 if ingredients_list:
  
-    ingredients_string = ''
+    ingredients_string = ' '
  
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
- 
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
-        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width = True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+ fruit_chosen)
+        fv_ = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
  
     #st.write(ingredients_string)
  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
-            values ('""" +ingredients_string+ """','"""+name_on_order+"""')"""
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
  
-    #st.write(my_insert_stmt)
+    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
  
     if time_to_insert:
